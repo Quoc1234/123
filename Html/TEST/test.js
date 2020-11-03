1fb0068f403b498a3db8c0e5124e09009bbbb1f2
Quoc1234 <you@example.com>
2020-11-03 20:25:07 +0700

    adcd

---
+++ Html/TEST/test.js
@@ -0,0 +1,10 @@
+let a = parseInt(prompt("Nhập vào số a: "));
+let b = parseInt(prompt("Nhập vào số b"));
+
+let tong = a + b;
+let hieu = a - b;
+let thuong = a/b;
+let tich  = a * b;
+
+document.write("tổng của 2 số a và b là " + " " + tong);
+
